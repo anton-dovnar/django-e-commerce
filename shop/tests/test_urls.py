88624540c768d1687e3f3c00999a8ac6416f82a4
Anton Dovnar <fode4cun@gmail.com>
2021-01-27 15:59:16 +0300

    Create product-detail url test

--- shop/tests/test_urls.py
+++ shop/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase, tag
 from django.urls import resolve, reverse
 
-from shop.views import ProductListView
+from shop.views import ProductListView, ProductDetailView
 
 
 @tag('shop-urls')
@@ -18,3 +18,11 @@ class UrlsTest(SimpleTestCase):
         )
 
         self.assertEqual(resolve(url).func.view_class, ProductListView)
+
+    def test_product_detail_url_resolves(self):
+        url = reverse(
+            'shop:product-detail',
+            args=[1, 'nike-sb-zoom-blazer-mid-edge']
+        )
+
+        self.assertEqual(resolve(url).func.view_class, ProductDetailView)
