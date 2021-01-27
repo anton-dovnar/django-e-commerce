95341c240ec09f8fedf36ac6573e0053090bc4b9
Anton Dovnar <fode4cun@gmail.com>
2021-01-27 14:35:14 +0300

    Create Category model / passing tests

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(
+        max_length=200, db_index=True, allow_unicode=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
