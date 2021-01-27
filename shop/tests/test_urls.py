efab3c1d1a6e4bf0c5062b1367bd4ce15299bcf5
Anton Dovnar <fode4cun@gmail.com>
2021-01-27 15:52:07 +0300

    Create ProductList urls test

---
+++ shop/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase, tag
+from django.urls import reverse, resolve
+
+from shop.views import ProductListView
+
+
+@tag('shop-urls')
+class UrlsTest(SimpleTestCase):
+    def test_product_list_url_resolves(self):
+        url = reverse('shop:product-list')
+
+        self.assertEqual(resolve(url).func.view_class, ProductListView)
+
+    def test_product_list_by_category_url_resolves(self):
+        url = reverse(
+            'shop:product-list-by-category',
+            args=['nike-sb-zoom-blazer-mid-edge']
+        )
+
+        self.assertEqual(resolve(url).func.view_class, ProductListView)
