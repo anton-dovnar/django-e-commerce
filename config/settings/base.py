6cb616d049d153c6a4e80d5fcd3f5f41173fee9c
Anton Dovnar <fode4cun@gmail.com>
2021-01-27 14:09:53 +0300

    Create layout templates / staticfiles

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Internal
+    'layout.apps.LayoutConfig',
 ]
 
 MIDDLEWARE = [
