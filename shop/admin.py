9da019f2a41b4bb6925f5e2f242195c2f09117cd
Anton Dovnar <fode4cun@gmail.com>
2021-01-30 18:28:01 +0300

    Create Photo / Size models
    Relation: ManyToOne to Prodcut.

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,41 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, Photo, Size
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'category',
+        'name',
+        'slug',
+        'description',
+        'price',
+        'available',
+        'created',
+        'updated',
+    )
+    list_filter = ('category', 'available', 'created', 'updated')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'image')
+    list_filter = ('product',)
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'size')
+    list_filter = ('product',)
--- shop/models.py
+++ shop/models.py
@@ -4,8 +4,7 @@ from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(
-        max_length=200, db_index=True, allow_unicode=True, unique=True)
+    slug = models.SlugField(max_length=200, db_index=True, allow_unicode=True, unique=True)
 
     class Meta:
         verbose_name = 'Category'
@@ -20,12 +19,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(
-        Category, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -42,3 +39,27 @@ class Product(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
+
+
+class Photo(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
+
+    def __str__(self):
+        return f'{self.product.name} - {self.image}'
+
+
+class Size(models.Model):
+    product = models.ForeignKey(Product, related_name='sizes', on_delete=models.CASCADE)
+    size = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Size'
+        verbose_name_plural = 'Sizes'
+
+    def __str__(self):
+        return f'{self.product.name} - {self.size}'
