c8f58dec2750499458582e0bba2d226997294827
Anton Dovnar <fode4cun@gmail.com>
2021-02-12 21:23:13 +0300

    Modify Order form
    Create order items while processing form.

--- order/forms.py
+++ order/forms.py
@@ -1,10 +1,29 @@
 from django import forms
 
-from shop.models import Order
+from cart.cart import Cart
+from order.models import Order, OrderItem
 
 
-class OrderCreateForm(forms.ModelForm):
+class OrderForm(forms.ModelForm):
 
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        cart = Cart(self.request)
+
+        if commit:
+            instance.save()
+
+        for item in cart:
+            OrderItem.objects.create(
+                order=instance, product=item['product'], price=item['price'], quantity=item['quantity'])
+
+        cart.clear()
+        self.request.session['order_id'] = instance.id
