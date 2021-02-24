1f35a6d4194b4bfd38aa7389313f05f410dce194
Anton Dovnar <fode4cun@gmail.com>
2021-02-24 13:39:15 +0300

    Setup send message backend

--- config/settings/base.py
+++ config/settings/base.py
@@ -146,3 +146,10 @@ BRAINTREE_CONF = braintree.Configuration(
     BRAINTREE_PUBLIC_KEY,
     BRAINTREE_PRIVATE_KEY
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
