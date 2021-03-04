a24eedef98af129c2dd962d503a8339070df24b3
Anton Dovnar <fode4cun@gmail.com>
2021-03-04 19:30:06 +0300

    Add order models tests

--- order/models.py
+++ order/models.py
@@ -4,8 +4,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from shop.models import Product
-
 
 class Order(models.Model):
     coupon = models.ForeignKey(
@@ -35,7 +33,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    product = models.ForeignKey('shop.Product', related_name='order_items', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
--- order/tests.py
+++ order/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from decimal import Decimal
 
-# Create your tests here.
+from django.test import TestCase, tag
+from model_mommy import mommy
+
+from order.models import Order, OrderItem
+
+
+@tag('order-models')
+class ModelsTest(TestCase):
+    def setUp(self):
+        self.order = mommy.make(Order)
+        self.order_item = mommy.make(OrderItem)
+
+    def test_order_creation(self):
+        self.assertIsInstance(self.order, Order)
+        self.assertEqual(self.order.__str__(), f'Order {self.order.id}')
+        self.assertIsInstance(self.order.get_total_cost(), Decimal)
+
+    def test_order_item_creation(self):
+        self.assertIsInstance(self.order_item, OrderItem)
+        self.assertEqual(self.order_item.__str__(), str(self.order_item.id))
+        self.assertIsInstance(self.order_item.get_cost(), Decimal)
