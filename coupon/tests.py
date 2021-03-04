36692340bd5bbfabf4ce8ce632666ab7185d8efc
Anton Dovnar <fode4cun@gmail.com>
2021-03-04 18:15:53 +0300

    Add coupon models tests

--- coupon/tests.py
+++ coupon/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, tag
+from model_mommy import mommy
 
-# Create your tests here.
+from coupon.models import Coupon
+
+
+@tag('coupon-models')
+class ModelsTest(TestCase):
+    def test_coupon_creation(self):
+        coupon = mommy.make(Coupon)
+        self.assertIsInstance(coupon, Coupon)
+        self.assertEqual(coupon.__str__(), coupon.code)
