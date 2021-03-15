335a71118df52b3dc73e0f2bca2532818b5d0ee3
Anton Dovnar <fode4cun@gmail.com>
2021-03-15 22:37:53 +0300

    Release v0.1

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DJANGO_SETTINGS_MODULE=config.settings.dev \
-SECRET_KEY='3p@)5@n!uw&tgx@vmthq*q%k&zu#-1y2%tx$eyx2j15)daht)7' \
+SECRET_KEY='' \
 DATABASE_HOST=postgres \
 REDIS_HOST=redis \
 CACHE_HOST=cache \
