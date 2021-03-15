dc23baa49b27b3e8abce522eed90775c4dd649ec
Anton Dovnar <fode4cun@gmail.com>
2021-03-15 21:44:34 +0300

    Add document for order views

--- app/order/views.py
+++ app/order/views.py
@@ -18,11 +18,21 @@ class OrderFormView(FormView):
     success_url = reverse_lazy('payment:process')
 
     def get_form_kwargs(self):
+        """
+        Put a cart to the form kwargs for getting cart items when saving order.
+        """
+
         kwargs = super().get_form_kwargs()
         kwargs['cart'] = Cart(self.request)
         return kwargs
 
     def form_valid(self, form):
+        """
+        Define order id to the session for future manipulations.
+        If the customer paid the order successfully, we will be sending a mail message.
+        For getting email address we need an order id thats why we putting order id to the seession.
+        """
+
         order_id = form.save()
         self.request.session['order_id'] = order_id
         return super().form_valid(form)
@@ -30,12 +40,21 @@ class OrderFormView(FormView):
 
 @staff_member_required
 def admin_order_detail(request, order_id):
+    """
+    Add custom order detail page for administrator.
+    This page contains a list of order items and order credentials.
+    """
+
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'order/order_detail.html', {'order': order})
 
 
 @staff_member_required
 def admin_order_pdf(request, order_id):
+    """
+    Generate invoice pdf.
+    """
+
     order = get_object_or_404(Order, id=order_id)
     html = render_to_string('order/pdf.html', {'order': order})
     response = HttpResponse(content_type='application/pdf')
