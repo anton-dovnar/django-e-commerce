fa7e587d56a6097dadbc52ea6bb0baa34e4cf407
Anton Dovnar <fode4cun@gmail.com>
2021-03-15 22:13:04 +0300

    Add documentation for url converter

--- app/layout/converters.py
+++ app/layout/converters.py
@@ -1,6 +1,13 @@
-# Custom path converters
+"""
+Custom path converters
+"""
+
 
 class UnicodeSlugConverter:
+    """
+    Unicode slug converter.
+    """
+
     regex = r'[-0-9_\w]+'
 
     def to_python(self, value):
