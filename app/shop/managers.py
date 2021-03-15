91c64dcd1feb0e34510b5f19e3ffcdeee496e1c7
Anton Dovnar <fode4cun@gmail.com>
2021-03-15 18:38:43 +0300

    Add documentation for shop managers

--- app/shop/managers.py
+++ app/shop/managers.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class ProductManager(models.Manager):
+    """
+    Setting usable fields and prefetch-related images for optimization database access.
+    """
+
     def list(self):
         first_photo = models.Prefetch('images', to_attr='first_photo')
         qs = self.only(
