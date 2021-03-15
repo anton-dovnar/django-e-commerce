bda65cc8de3da020a999ebadfca88b3b7855042c
Anton Dovnar <fode4cun@gmail.com>
2021-03-15 22:04:02 +0300

    Add documentation for payment tasks

--- app/payment/tasks.py
+++ app/payment/tasks.py
@@ -12,7 +12,7 @@ from order.models import Order
 @shared_task
 def payment_completed(order_id):
     """
-    Task to send an e-mail notifications when an order is successfully created.
+    The task to send an e-mail notification when an order is successfully created.
     """
 
     order = Order.objects.get(pk=order_id)
@@ -20,12 +20,12 @@ def payment_completed(order_id):
     message = 'Please, find attached the invoice for your recent purchase.'
     email = EmailMessage(subject, message, settings.EMAIL_HOST_USER, [order.email])
 
-    # generate PDF
+    # Generate PDF
     html = render_to_string('order/pdf.html', {'order': order})
     out = BytesIO()
     css_path = settings.BASE_DIR.joinpath('order', 'static', 'order', 'css', 'pdf.css')
     weasyprint.HTML(string=html).write_pdf(out, stylesheets=[weasyprint.CSS(css_path)])
 
-    # attach PDF file / send
+    # Attach PDF file / send
     email.attach(f'order_{order_id}.pdf', out.getvalue(), 'application/pdf')
     email.send()
