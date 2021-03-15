ce1bb966a19eded7110a40f590100bc27caf1f76
Anton Dovnar <fode4cun@gmail.com>
2021-03-15 22:12:13 +0300

    Add docs for catigories context proceessor

--- app/layout/context_processors.py
+++ app/layout/context_processors.py
@@ -2,4 +2,9 @@ from shop.models import Category
 
 
 def categories(request):
+    """
+    List available categories for sidebar.
+    """
+
+    # TODO cache query
     return {'categories': Category.objects.all()}
