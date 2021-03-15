8da2f01846d577922fbf8e1ae45c086e404d04b0
Anton Dovnar <fode4cun@gmail.com>
2021-03-15 19:42:49 +0300

    Add documentation for shop views

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,3 +1,10 @@
+"""
+Currently support the follow two views:
+
+1. **Product List** - list available products [[views.py#list]].
+2. **Product Detail** - detail product page [[views.py#detail]].
+"""
+
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
@@ -7,11 +14,16 @@ from shop.models import Category, Product
 from shop.recommender import Recommender
 
 
+# === List ===
 class ProductListView(ListView):
     model = Product
     paginate_by = 8
 
     def get_queryset(self):
+        """
+        Filter products by Category.
+        """
+
         category = None
         category_slug = self.kwargs.get('category_slug', None)
         products = self.model.objects.list()
@@ -27,17 +39,27 @@ class ProductListView(ListView):
         return products
 
     def render_to_response(self, context, **response_kwargs):
+        """
+        Infinite scroll with ajax request.
+        """
+
         if self.request.is_ajax():
             return render(self.request, 'shop/product_list_ajax.html', context)
 
         return super().render_to_response(context, **response_kwargs)
 
 
+# === Detail ===
 class ProductDetailView(DetailView):
     model = Product
     query_pk_and_slug = True
 
     def get_context_data(self, **kwargs):
+        """
+        Add product to cart.
+        Suggest products for this product [[recommender.py#suggest_products_for]].
+        """
+
         context = super().get_context_data(**kwargs)
         product = super().get_object()
         r = Recommender()
